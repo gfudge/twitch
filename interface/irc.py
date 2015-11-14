f5d6e4f0a946eef7725c4f715b251c2ec997db4f
Ubuntu <ubuntu@ip-172-31-24-79.eu-west-1.compute.internal>
2015-11-14 17:39:34 +0000

    added irc stuff and yields

--- interface/irc.py
+++ interface/irc.py
@@ -3,22 +3,36 @@ import sys
 
 class irc:
 
-	def __init__(self, host, port, max_bytes=100):
-		self.irc = socket.socket()
+	def __init__(self, host, port, nick, ident, realname, max_bytes=1024):
+		self.socket = socket.socket()
 		self.max_bytes = max_bytes
 		self.host = host
 		self.port = port
+		self.nick = nick
+		self.ident = ident
+		self.realname = realname
+		self.readbuffer = []
 	
 	def connect(self):
-		self.irc.connect((self.host,self.port))
-	
+		self.socket.connect((self.host,self.port))
+		self.socket.send("NICK %s\r\n" % nick)
+		self.socket.send("USER %s %s bla :%s\r\n" % (ident, host, realname))
+		
 	def readline(self):
+		self.readbuffer = self.readbuffer + self.socket.recv(1024).splitlines()	
+		yield self.readbuffer.pop(0)
+
+	def readline2(self):
 		chunks = []
 		bytes_recv = 0
 		while bytes_recv < self.max_bytes:
-			chunk = self.irc.recv()
+			chunk = self.socket.recv()
 			if chunk == '':
 				raise RuntimeError("Socket broken")
 			chunks.append(chunk)
 			bytes_recv = bytes_recv + len(chunk)
 		return ''.join(chunks)
+
+	def start(self):
+		self.connect()
+		
